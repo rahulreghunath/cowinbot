1df909d220f545d12a4d2feeef70b0a72f038b50
Rahul <rahulreghunath11@gmail.com>
2021-05-24 18:33:32 +0530

    all districts added

--- bot.py
+++ bot.py
@@ -9,9 +9,10 @@ from telegram.ext import (
     ConversationHandler,
     CallbackContext, CallbackQueryHandler,
 )
-import calander
+from districts import districts
 from dotenv import load_dotenv
 import os
+
 # Enable logging
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
@@ -26,69 +27,6 @@ GENDER, PHOTO, LOCATION, BIO, SELECT_TYPE, PIN, DISTRICT_RESULT, PIN_RESULT, RES
 user_district = None
 user_pin = None
 user_date = None
-
-districts = [
-    {
-        "district_id": 301,
-        "district_name": "Alappuzha"
-    },
-    {
-        "district_id": 307,
-        "district_name": "Ernakulam"
-    },
-    {
-        "district_id": 306,
-        "district_name": "Idukki"
-    },
-    {
-        "district_id": 297,
-        "district_name": "Kannur"
-    },
-    {
-        "district_id": 295,
-        "district_name": "Kasaragod"
-    },
-    {
-        "district_id": 298,
-        "district_name": "Kollam"
-    },
-    {
-        "district_id": 304,
-        "district_name": "Kottayam"
-    },
-    {
-        "district_id": 305,
-        "district_name": "Kozhikode"
-    },
-    {
-        "district_id": 302,
-        "district_name": "Malappuram"
-    },
-    {
-        "district_id": 308,
-        "district_name": "Palakkad"
-    },
-    {
-        "district_id": 300,
-        "district_name": "Pathanamthitta"
-    },
-    {
-        "district_id": 296,
-        "district_name": "Thiruvananthapuram"
-    },
-    {
-        "district_id": 303,
-        "district_name": "Thrissur"
-    },
-    {
-        "district_id": 299,
-        "district_name": "Wayanad"
-    },
-    {
-        "district_id": 12,
-        "district_name": "Prakasam"
-    }
-]
 
 
 def get_data(selected_district=None, date=None, pin=None):
